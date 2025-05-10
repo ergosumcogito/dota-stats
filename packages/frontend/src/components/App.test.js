fab6b3719179004645182a589c02f0ff0bb68b63
ergosumcogito <ergosumcogito@users.noreply.github.com>
2025-05-10 21:57:33 +0200

    Add simple home page, remove test project code

--- packages/frontend/src/components/App.test.js
+++ packages/frontend/src/components/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from '../pages/Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders welcome message', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Welcome to Dota 2 Stats/i);
   expect(linkElement).toBeInTheDocument();
 });
