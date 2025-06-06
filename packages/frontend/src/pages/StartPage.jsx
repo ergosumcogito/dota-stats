c071029ed2db0476fbe85d46aa6a380f5d729e7e
ergosumcogito <ergosumcogito@users.noreply.github.com>
2025-06-06 21:53:56 +0200

    Finish StartPage.jsx

--- packages/frontend/src/pages/StartPage.jsx
+++ packages/frontend/src/pages/StartPage.jsx
@@ -4,10 +4,14 @@ import logo from 'assets/logo.png';
 export default function StartPage() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
-            <img src={logo} className="w-72 h-72 object-contain" alt="Logo"/>
-            <h1 className="text-5xl font-sans font-[800] text-text mb-2">Dota Stats</h1>
-            <h2 className="text-xl font-sans font-[400] text-primary mb-12">Know the meta. Become <em className="italic">undefeatable</em>.</h2>
-            <nav className="space-x-4">
+            <img
+                src={logo}
+                className=" w-48 h-auto sm:w-56 md:w-72"
+                alt="Logo"/>
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-sans font-[800] text-text mb-2">Dota Stats</h1>
+            <h2 className="text-base sm:text-xl md:text-2xl
+            font-sans font-[400] text-primary mb-12">Know the meta. Become <em className="italic">undefeatable</em>.</h2>
+            <nav className="flex flex-wrap justify-center gap-4">
                 <Link to="/player-search" className="btn start-btn-player">
                     Player's Profile
                 </Link>
