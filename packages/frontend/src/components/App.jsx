ddb147cfbc1cd5cbb173012564b4d06f0828826e
davidspringean12 <springeandavid5@gmail.com>
2025-07-26 20:50:39 +0200

    Implement hero list

--- packages/frontend/src/components/App.jsx
+++ packages/frontend/src/components/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartPage from 'pages/StartPage';
 import PlayerProfilePage from "pages/PlayerProfilePage";
-//import HeroList from './pages/HeroList';
+import HeroList from 'pages/HeroList';
 //import HeroMatrix from './pages/HeroMatrix';
 
 function App() {
     return (
         <Router>
-            <div className="max-w-5xl w-full mx-auto px-4 py-3">
+            <div className="w-full mx-auto px-4 py-3">
             <Routes>
                 <Route path="/" element={<StartPage />} />
                 <Route path="/player-search" element={<PlayerProfilePage />} />
-                {/*<Route path="/hero-list" element={<HeroList />} />*/}
+                <Route path="/hero-list" element={<HeroList />} />
                 {/*<Route path="/hero-matrix" element={<HeroMatrix />} />*/}
             </Routes>
             </div>
