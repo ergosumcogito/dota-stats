25e701bebd8df6ceac5d4cf5008d3af481b0894c
ergosumcogito <ergosumcogito@users.noreply.github.com>
2025-07-29 01:37:38 +0200

    Fix App.jsx

--- packages/frontend/src/components/App.jsx
+++ packages/frontend/src/components/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartPage from 'pages/StartPage';
 import PlayerProfilePage from 'pages/PlayerProfilePage';
 import Hero from 'pages/Hero';
+import NotFoundPage from "pages/NotFoundPage";
+import HeroList from "pages/HeroList";
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
           <Routes>
           <Route path="/" element={<StartPage />} />
             <Route path="/player-search/:playerId?" element={<PlayerProfilePage />} />
-          <Route path="/hero" element={<Hero />} />
+            <Route path="/hero-list" element={<HeroList />} />
+            <Route path="/hero" element={<Hero />} />
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
