16a9e6a8f55fa7601ad11a7164556b7f4265c220
ergosumcogito <ergosumcogito@users.noreply.github.com>
2025-07-29 15:45:56 +0200

    Make comment font bold, add 404 emoji

--- packages/frontend/src/pages/NotFoundPage.jsx
+++ packages/frontend/src/pages/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 function NotFoundPage() {
     return (
         <div className="text-center mt-20">
-            <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+            <h1 className="text-4xl font-bold mb-4">404 - Page Not Found 😶‍🌫️</h1>
             <p className="text-text">The page you're looking for doesn't exist.</p>
         </div>
     );
