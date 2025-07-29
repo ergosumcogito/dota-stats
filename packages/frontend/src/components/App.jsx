cb2cd43861fa3bef544ccf8fecd98dfd18f41a1e
davidspringean12 <springeandavid5@gmail.com>
2025-07-29 10:46:04 +0200

    Implemented Navbar in router

--- packages/frontend/src/components/App.jsx
+++ packages/frontend/src/components/App.jsx
@@ -4,20 +4,22 @@ import PlayerProfilePage from 'pages/PlayerProfilePage';
 import Hero from 'pages/Hero';
 import NotFoundPage from "pages/NotFoundPage";
 import HeroList from "pages/HeroList";
+import Navbar from 'components/NavBar';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-[#0a192f] text-white">
-          {/*<div className="w-full mx-auto px-4 py-3">*/}
-          <Routes>
-          <Route path="/" element={<StartPage />} />
-            <Route path="/player-search/:playerId?" element={<PlayerProfilePage />} />
-            <Route path="/hero-list" element={<HeroList />} />
-            <Route path="/hero" element={<Hero />} />
-            <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
+       <Navbar />
+          <div className="min-h-screen bg-[#0a192f] text-white">
+              {/*<div className="w-full mx-auto px-4 py-3">*/}
+              <Routes>
+              <Route path="/" element={<StartPage />} />
+                <Route path="/player-search/:playerId?" element={<PlayerProfilePage />} />
+                <Route path="/hero-list" element={<HeroList />} />
+                <Route path="/hero" element={<Hero />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </div>
     </Router>
   );
 }
